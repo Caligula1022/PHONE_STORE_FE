c140a20722371b44f1c9c19b16e1281ffcfe5561
Yifei Tai <89871210+Caligula1022@users.noreply.github.com>
2022-12-13 13:47:48 -0500

    comment interceptpor

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -11,15 +11,15 @@ const API = axios.create({
   withCredentials: true,
 });
 
-API.interceptors.request.use(
-  function (req) {
-    const token = JSON.parse(localStorage.getItem('token'));
-    if (token) req.headers['auth-token'] = token;
-    return req;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
-);
+// API.interceptors.request.use(
+//   function (req) {
+//     const token = JSON.parse(localStorage.getItem('token'));
+//     if (token) req.headers['auth-token'] = token;
+//     return req;
+//   },
+//   function (error) {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default API;
